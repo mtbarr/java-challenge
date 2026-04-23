e9085a555e11b770e3993b59e1127d5d8b12552a
mtbarr <mtbarrdev@gmail.com>
2026-04-23 16:54:27 -0300

    chore: initial project setup

---
+++ src/main/java/io/github/mtbarr/challenge/JavaChallengeApplication.java
@@ -0,0 +1,12 @@
+package io.github.mtbarr.challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaChallengeApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(JavaChallengeApplication.class, args);
+    }
+}
